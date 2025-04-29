d926569d7306856b5f2a8cf84a871444524a76f9
VasanthaKanniah <vasanthak@Mac-200.lan>
2025-04-29 14:54:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const App = () => {
     <div className="App">
       <Confetti />
       <header className="App-header">
-        <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
+        <h1 style={{ marginBottom: "0px" }}>Congratulations ***</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first container.
+          You ran your first container...
         </p>
         <div>
           <a
